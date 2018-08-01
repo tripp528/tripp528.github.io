d2cba22022e6594a6085fba42c660b24148b7f84
tripp528 <tripp528@gmail.com>
2018-08-01 13:36:30 -0600

    reverted back to html

--- php/content.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-
-<div id="index-banner" class="parallax-container">
-  <?php include 'php/form2/structure.php'; ?>
-
-  <div class="parallax"><img src="img/background1.jpg" alt="Unsplashed background img 1"></div>
-</div>
